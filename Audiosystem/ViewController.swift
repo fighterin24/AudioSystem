3eb49b70d58a66a3ce67c0bee73564c20c780a7d
Monika <monika_pols@hotmail.de>
2018-01-09 20:35:16 +0100

    Initial Commit

---
+++ Audiosystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Audiosystem
+//
+//  Created by Monika Schepan on 09.01.18.
+//  Copyright © 2018 Monika Schepan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
